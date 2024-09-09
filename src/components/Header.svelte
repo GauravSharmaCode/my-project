36c561d841513f4cb893fe6efaf8613aac6b15de
GauravSharmaCode <72681559+GauravSharmaCode@users.noreply.github.com>
2024-09-09 22:23:29 +0530

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,10 +2,9 @@
     export let y;
 
     export let tabs = [
-        { name: "Projects ", link: "#projects" },
+        { name: "Projects", link: "#projects" },
         { name: "About me", link: "#about" },
-        // { name: "Contact", link: "" },
-        // {name: '', link: ''},
+        { name: "Blogs", link: "https://gauravs-blogs.onrender.com" } // New Blogs tab
     ];
 </script>
 
@@ -23,19 +22,11 @@
             <a
                 href={tab.link}
                 class="duration-200 hover:text-violet-400"
-                target={index === 2 ? "_blank" : ""}
+                target={index === 2 ? "_blank" : ""}  <!-- Add target="_blank" for Blogs tab to open in a new tab -->
             >
                 <p>{tab.name}</p>
             </a>
         {/each}
-        <!-- <button
-            class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-violet-600 text-white"
-        >
-            <div
-                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
-            />
-            <h4 class="relative z-9">Login</h4>
-        </button> -->
         <button
             class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
         > <a href="#contact">
